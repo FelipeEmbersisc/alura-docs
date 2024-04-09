93f3130fa6ccfe0a6ed3c9d53d5b12be507e324b
FelipePanizza <felipe.panizza@poupacred.com.br>
2024-04-09 17:43:25 -0300

    feat: iniciando o servidor node + socket.io

---
+++ src/servidor.js
@@ -0,0 +1,22 @@
+import express from "express";
+import http from "node:http";
+import url from "node:url";
+import path from "node:path";
+import { Server } from "socket.io";
+
+const porta = process.env.PORTA || 3000;
+
+const app = express();
+
+const caminhoAtual = url.fileURLToPath(import.meta.url);
+const diretorioPublico = path.join(caminhoAtual, "../..", "public");
+
+app.use(express.static(diretorioPublico));
+
+const servidorHttp = http.createServer(app);
+servidorHttp.listen(porta,() => console.log(`Servidor escutando na porta ${porta}`));
+
+const io = new Server(servidorHttp);
+io.on("connection", () => {
+  console.log("Um cliente se conectou.");
+});
